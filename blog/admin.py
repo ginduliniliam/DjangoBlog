9eea930124228cbcf918380ac69d34dc13137aac
Gindulin Ilia <gindulin.gho@gmail.com>
2024-03-02 23:52:06 +0300

    Register Post in admin and create in file admin class PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']  # shows a list of objects on the page
+    list_filter = ['status', 'created', 'publish', 'author']  # filters results for fields included in the attribute
+    search_fields = ['title', 'body']  # Search options
+    prepopulated_fields = {'slug': ('title',)}  # title written in slug
+    raw_id_fields = ['author']  # to select the list automatically.
+    date_hierarchy = 'publish'  # navigation by publication dates
+    ordering = ['status', 'publish']  # default sorting
